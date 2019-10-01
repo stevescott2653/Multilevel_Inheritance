8cef17b03b925969c0c8a8dfe1e805387f5ea739
Steven Scott <stevescott2653@gmail.com>
2019-10-01 16:02:55 -0400

    Add files via upload

---
+++ Multilevel_Inheritance/Multilevel_Inheritance/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multilevel_Inheritance
+{
+    public class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Eating...");
+        }
+
+        /// <summary>
+        /// The dog.
+        /// </summary>
+        public class Dog: Animal
+        {
+            public void Bark()
+            {
+                Console.WriteLine("Barking...");
+            }
+        }
+
+        public class BabyDog : Dog
+        {
+            public void Weep()
+            {
+                Console.WriteLine("Weeping...");
+            }
+        }
+
+        class TestInheritance2
+        {
+            public static void Main(string[] args)
+            {
+                BabyDog d1 = new BabyDog();
+                d1.Eat();
+                d1.Bark();
+                d1.Weep();
+            }
+        }
+    }
+}
